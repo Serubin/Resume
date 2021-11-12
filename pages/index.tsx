643fbb12598dae0520b08a67253855bc8c393492
Serubin <Serubin@serubin.net>
2021-11-11 22:26:11 -0800

    Update types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import type { NextPage } from 'next'
 import getStaticContent from '../lib/contentData';
 import Resume from '../components/resume';
+import type { Resume as ResumeType } from '../lib/types';
 
-const Index: NextPage = ({ resume }) => {
+type Props = {
+  resume: ResumeType;
+};
+
+const Index: NextPage<Props> = ({ resume }) => {
   return (
     <Resume resume={resume}/>
   )
 }
 
 export const getStaticProps = async () => {
-  const data = getStaticContent(['resume']);
+  const data: any = getStaticContent(['resume']);
 
   return {
     props: {
